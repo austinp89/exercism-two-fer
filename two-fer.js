8a78fa78fbe432d59e15adb342967a8fb3321463
austin <44382233+austinp89@users.noreply.github.com>
2018-11-15 09:30:44 -0700

    Add files via upload

---
+++ two-fer.js
@@ -0,0 +1,9 @@
+const twoFer = (name) => {
+    if(name === ''){
+    return 'One for you, one for me.' }
+    else if(`${name}`)
+    return `One for ${name}, one for me.`}
+
+module.exports = {
+    twoFer
+}
